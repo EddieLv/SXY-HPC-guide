ff1c15cdd0f1e5864c54c2f5f6f21510f55a1de8
biogenger <2849287458@qq.com>
2025-08-12 15:44:18 +0800

    Init commit

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,23 @@ release = 'v1.0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'myst_parser',
+    "sphinxcontrib.mermaid",
+    "sphinx_copybutton",
+]
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
+
+myst_enable_extensions = [
+    "tasklist",
+    "deflist",
+    "dollarmath",
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -26,3 +42,21 @@ language = 'zh_CN'
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
+html_theme_options = {
+    'analytics_anonymize_ip': False,
+    'logo_only': True,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False,
+}
+html_logo = "./_static/logo.png"
+html_js_files = [
+    'my_custom.js',
+]
+
+
